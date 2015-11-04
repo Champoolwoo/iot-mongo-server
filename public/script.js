1bd8c6ac055f8d6969e1ed1f6a667ff924064b3c
Champoolwoo <golf_zet@hotmail.com>
2015-11-04 18:24:20 +0700

    สร้างกราฟ แต่ไม่ขึ้น กำลังแก้ปัญหา

--- public/script.js
+++ public/script.js
@@ -34,4 +34,49 @@ angular.module('app', [])
           alert(response.data.message)
         })
     }
+    ////////////////////////////////////
+    vm.graph = function(){
+      $http.get('/api/iot')
+      .then(function success (response) {
+        var data = {
+        labels : [],
+        datasets : [
+          {
+            label: "temperature",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: []
+          },
+          {
+            label: "relative_humidity",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: []
+          }]
+        }
+
+        var ctx = document.getElementById("myChart").getContext("2d");
+        new Chart(ctx).Line(data);
+
+        for(var i =0;i<response.data.length;i++){
+          if (response.data[i].iot_id==0){
+            myLineChart.addData([response.data[i].temperature, response.data[i].relative_humidity] ,vm.toThaiDateTime(response.data[i].timestamp));
+          }
+                   
+        }
+
+      },function error (response) {
+        alert(response.data.message)
+      }) 
+    }
+    ////////////////////////////////////
+
   })
